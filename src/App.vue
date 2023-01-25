80edcbf5de5daa4ebe39d92ea83ae7b0cc129fec
puweb-mtis <104874657+puweb-mtis@users.noreply.github.com>
2023-01-25 16:58:22 +0900

    Update

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+// Components
+import Header from '@/components/Header.vue'
+</script>
+
+<template>
+  <Header />
+
+  <main></main>
+</template>
+
+<style lang="scss">
+  ul, ol, dl {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+</style>
